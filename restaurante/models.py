08abcdd7b4d162b5407bc368b59802142e1edfbf
samuel fontes(pc) <samuelfontess05@gmail.com>
2025-09-18 09:55:03 -0300

    atualização adicionado preço no cardapio

--- restaurante/models.py
+++ restaurante/models.py
@@ -23,6 +23,7 @@ class CardapioItem(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField()
     foto = models.ImageField(upload_to='cardapio/')
+    preco = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
         return self.titulo
