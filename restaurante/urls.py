a36a3657fc6bf049747fa588748547bcb6b731c5
Cledianealencar <mariaclediane2512@gmail.com>
2025-09-20 17:06:29 -0300

    adicionando endpoints de reserva e fale conosco

--- restaurante/urls.py
+++ restaurante/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('cardapio/', views.cardapio, name='cardapio'),
     path('', views.equipe, name='equipe'),
     path('historia/', views.feedbacks, name='feedbacks'),
+    path('reservas/', views.criar_reserva, name='criar_reserva'),
+    path('faleconosco/', views.fale_conosco, name='fale_conosco'),
 ]
