49d5e266740f47b706ed3b12587f073f47b13c2d
samuel fontes(pc) <samuelfontess05@gmail.com>
2025-09-20 15:01:52 -0300

    atualização feedback do cliente + Nossa equipe + atualização banco de dados com imagem e preços

--- restaurante/admin.py
+++ restaurante/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import PromocaoEvento, CardapioItem
+from .models import PromocaoEvento, CardapioItem, Funcionario, FeedbackCliente
 
 admin.site.register(PromocaoEvento)
 admin.site.register(CardapioItem)
+admin.site.register(Funcionario)
+admin.site.register(FeedbackCliente)
